8aae17ebfd5c09510ae7c60796544a17a837255f
nahiefhspc <t.al.khackheist@gmail.com>
2025-05-24 16:43:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -447,7 +447,7 @@ async def handle_topic_selection(update: Update, context: ContextTypes.DEFAULT_T
                 logger.info(f"Processing topic {topic_count}/{total_topics}: {topic.get('name', 'No Name')}")
                 topic_content = collect_topic_contents(topic, selected_subject, batch_id, token)
                 if topic_content:
-                    f.write(f"Topic: {topic.get('name', 'No Name')}\n{topic_content}\n\n")
+                    f.write(f"{topic_content}\n")
                     content_written = True
                     f.flush()
                     logger.info(f"Written content for topic {topic.get('name', 'No Name')}")
@@ -514,9 +514,7 @@ async def handle_topic_selection(update: Update, context: ContextTypes.DEFAULT_T
 
 async def main():
     # Replace with your actual bot token
-    application = Application.builder().token("7549640350:AAFp-7vzfhRIo856b-f_gEilKIoeS9KPL5E").build()
-
-    # Register handlers
+    application = Application.builder().token("8110893329:AAGhPnxf0dk4WaXvMPrnEZRlbveJyrntizk").b  # Register handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("batch_id", batch_id))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
